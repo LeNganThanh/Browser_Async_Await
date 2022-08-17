c7e3cca3f280130eb2cb38079d75753d77362831
Thanh Le <99728087+LeNganThanh@users.noreply.github.com>
2022-08-17 14:32:40 +0200

    first async/await commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,9 @@
 import "../styles/main.scss";
-import "babel-polyfill"
-
+import "babel-polyfill";
 const modal = document.getElementById("myModal");
+
+/* the original code----
+ const modal = document.getElementById("myModal");
 let promiseOfModal = new Promise(function (resolve) {
     window.setTimeout(function () {
         resolve(modal)
@@ -20,5 +22,75 @@ modal.addEventListener("click", (e) => {
         modal.style.display = "none"
         break;
     }
-})
+}) */
 
+//close button
+modal.addEventListener("click", e => {
+  switch (e.target.className) {
+    case "close":
+    case "modal":
+      modal.style.display = "none";
+      break;
+  }
+});
+
+//using async-await syntax-----------------
+
+/* another way
+ async function getModalPromise() {
+  let promiseOfModal = new Promise(function (resolve) {
+    window.setTimeout(function () {
+      resolve((modal.style.display = "block"));
+    }, 1000 * 60);
+  });
+
+  let myModal = await promiseOfModal;
+  return myModal;
+}
+getModalPromise(); */
+
+function promiseOfModal() {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve((modal.style.display = "block"));
+    }, 1000 * 60);
+  });
+}
+async function getModalPromise() {
+  let myModal = await promiseOfModal();
+  return myModal;
+}
+
+getModalPromise();
+
+//------Animation end-----------
+const continueBtn = document.getElementById("continue");
+continueBtn.addEventListener("mouseleave", function () {
+  continueBtn.style.backgroundColor = "darkcyan";
+  continueBtn.style.color = "white";
+  async function alertMsg() {
+    let getMsg = new Promise((resolve, reject) => {
+      setTimeout(function () {
+        resolve(alert("Continue to subscribe!"));
+      }, 200);
+    });
+    let asyncAlert = await getMsg;
+    return asyncAlert;
+  }
+  alertMsg();
+});
+
+/* another way----
+  function alertMsg() {
+    return new Promise((resolve, reject) => {
+      setTimeout(function () {
+        resolve(alert("Continue to subscribe!"));
+      }, 200);
+    });
+  }
+  async function getAlert() {
+    let asyncAlert = await alertMsg();
+    return asyncAlert;
+  }
+  getAlert();
+*/
